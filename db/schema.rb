2e8eddf089f2b51c147f68e8e2a58f784ae2dbc3
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-09-11 19:30:40 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,24 @@ ActiveRecord::Schema.define(version: 2019_09_06_112204) do
     t.string "image"
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_kana_name", null: false
+    t.string "first_kana_name", null: false
+    t.date "birthday", null: false
+    t.integer "tel", null: false
+    t.text "profile"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
