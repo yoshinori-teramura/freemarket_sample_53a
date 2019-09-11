8842cbb581195ec4a8345b2b2b2e7182ccdb63e0
tokidoki-53 <v.asurada.11@gmail.com>
2019-09-11 15:44:03 +0900

    S3の画像の保存先【01】

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,12 @@ CarrierWave.configure do |config|
     config.storage = :file
   elsif Rails.env.production?
     config.storage = :fog
-    config.fog_provider = 'fog/aws'    
+    config.fog_provider = 'fog/aws'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
     }
 
