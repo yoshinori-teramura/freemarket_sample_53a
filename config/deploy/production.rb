0f887122a5787c750c663d59df1978e02f229ed0
tokidoki-53 <v.asurada.11@gmail.com>
2019-09-12 23:21:30 +0900

    Basic認証を実装

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@ server '18.179.68.176', user: 'ec2-user', roles: %w{app db web} #, my_property: 
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
