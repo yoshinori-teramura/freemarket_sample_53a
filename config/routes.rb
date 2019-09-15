5d67afcdebf917a80853a6d89fbf1742a95d10f8
skysan87 <skysan.justacase@gmail.com>
2019-09-15 17:31:21 +0900

    generate mypage controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :mypages, only: :index
   root 'users#index'
 end
