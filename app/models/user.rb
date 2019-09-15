fdb18f13765abdf041105f0d2f8162f796a8a8ae
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-09-15 16:52:35 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :items
-  has_many :addresses       
+  has_many :addresses
+  has_one  :credit
+         
 end
