af410ce850d9bef6a151d2da4427eab4d7cbd065
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-09-15 16:52:29 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
 
-  #  before_action :authenticate_user!
+   before_action :authenticate_user! excpt: :index
 
+  
   def index
     
   end
+
+
+
 end
