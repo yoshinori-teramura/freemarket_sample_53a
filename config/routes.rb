61d42a6ff28cee0dec780d4ba0703dd3a68cfceb
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-09-22 18:18:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
+  get "users/registration" => "users#registration"
 end
