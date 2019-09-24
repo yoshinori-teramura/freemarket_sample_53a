f2c530ae0820149a0e98e51081f4f0f4416455bc
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-09-24 20:53:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   resources :items
   resources :mypages, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "users/signup" => "users#signup"
-  get "users/registration" => "users#registration"
-  get "users/sms_confirmation" => "users#sms_confirmation"
+  get "users/users/registration" => "users#registration"
+  get "users/users/users/sms_confirmation" => "users#sms_confirmation"
   get "users/address" => "users#address"
   get "users/credit" => "users#credit"
   get "users/complete" => "users#complete"
