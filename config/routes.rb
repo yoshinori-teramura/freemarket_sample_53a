666ad8ae0973956d1d9d245d5e27a0970077fff0
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-09-25 02:38:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   resources :items
   resources :mypages, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   get "users/address" => "users#address"
   get "users/credit" => "users#credit"
   get "users/complete" => "users#complete"
-  get "users/log_in" => "users#log_in"
+  get "users/login" => "users#login"
+  get "users/sign_up" => "devise/registrations#new"
 
-  root 'items#index'
+  root 'devise/registrations#new'
 end
