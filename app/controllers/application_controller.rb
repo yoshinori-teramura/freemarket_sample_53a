1ef628d41934cc01d91eeb9f8e77803348d750ed
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-09-26 15:34:57 +0900

    Revert "deviseのデータベース保存機能"
    This reverts commit 1dc3b703998c6bac93324e9d92411e626b625f72.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-  before_action :configure_permited_parameters, if: :devise_controller?
 
-  def configure_permited_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_kana_name, :first_kana_name, :birthday, :tel])
-  end
-  
   private
 
   def production?
