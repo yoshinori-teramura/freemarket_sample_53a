deff931e74b10ab5eaad43470aa533670cc646f4
skysan87 <skysan.justacase@gmail.com>
2019-10-11 08:12:04 +0900

    戻るボタン実装

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,3 +1,8 @@
-$(function () {
-  console.log("sell.js loaded");
+$(document).on('turbolinks:load', function () {
+
+  // 別ページから遷移すると使用できない
+  $('#sell-back-btn').on('click', function(e) {
+    e.preventDefault();
+    window.history.back();
+  });
 });
