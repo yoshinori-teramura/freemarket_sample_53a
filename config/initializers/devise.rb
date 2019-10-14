1253ff97e8841d3d7bf6269d5e20e5403f1ea7af
Shinichiro Miyazono <shinichiro.miyazono@gmail.com>
2019-10-14 17:09:35 +0900

    Update devise.rb & registration_type.html.haml
    変数の名前修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -300,5 +300,5 @@ Devise.setup do |config|
   
   
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],{}
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY']
 end
