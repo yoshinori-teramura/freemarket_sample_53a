9a601d6023bf11fd737def2444dd4abf94e25f6a
skysan87 <skysan.justacase@gmail.com>
2019-10-14 19:02:30 +0900

    商品を保存できるように

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -5,7 +5,15 @@ class SellController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
     # FIXME: 仮置き
     redirect_to :root
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:description, :image)
   end
 end
