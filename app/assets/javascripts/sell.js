1b4133979f032eed1631508ade3a7eef99d00cfb
skysan87 <skysan.justacase@gmail.com>
2019-10-15 01:04:09 +0900

    価格計算を実装

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,9 +1,35 @@
 $(document).on('turbolinks:load', function () {
 
-  console.log("sell.js loaded");
-
   $('#sell-back-btn').on('click', function (e) {
     e.preventDefault();
     window.history.back();
   });
+
+  $('#sell-input-price').on('keyup', function (e) {
+    var input = this.value;
+
+    if (input.length === 0 || isNaN(input)) {
+      resetSellPrice();
+      return false;
+    }
+
+    if(input < 300 || 9999999 < input) {
+      resetSellPrice();
+      return false;
+    }
+
+    var price = Number(input);
+    var tax = Math.round(price / 10);
+    var profit = price - tax;
+
+    $(".sell-form-price__tax--right").text('¥' + tax.toLocaleString());
+    $(".sell-form-price__profit--right").text('¥' + profit.toLocaleString());
+
+  });
+
+  function resetSellPrice() {
+    $(".sell-form-price__tax--right").text("-");
+    $(".sell-form-price__profit--right").text("-");
+  }
+
 });
