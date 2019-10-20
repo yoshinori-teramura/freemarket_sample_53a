5d9dcb2095f649690210accf47a81b6ad66070d2
skysan87 <skysan.justacase@gmail.com>
2019-10-20 19:01:08 +0900

    カテゴリ追加

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -2,12 +2,18 @@ class SellController < ApplicationController
 
   def index
     @item = Item.new
+    @categories = []
+    @categories << ["---", 0]
+    #データベースから、Rootカテゴリーのみ抽出し、配列化
+    Category.where(ancestry: nil).each do |root|
+      @categories << [root.name, root.id]
+    end
   end
 
   def create
     @item = Item.new(item_params)
     @item.save
-    # FIXME: 仮置き
+    # TODO:出品完了ページへ遷移
     redirect_to :root, notice: 'Item was successfully created.'
   end
 
