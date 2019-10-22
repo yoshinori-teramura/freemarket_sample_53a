8cf4fd546b57ed4622e8a1fa740fff24c02ae189
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-10-22 23:33:32 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -74,6 +74,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :image, :price)
+      params.require(:item).permit(:name, :description, :image, :price, :user_id, :category_id)
     end
 end
