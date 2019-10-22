72085bdd15be972e3657be8693d63fe43743e7a8
skysan87 <skysan.justacase@gmail.com>
2019-10-22 13:02:26 +0900

    トランザクションを設定。商品登録に失敗した場合、ブランドも登録できないように処理。

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -13,32 +13,36 @@ class SellController < ApplicationController
 
   def create
 
-    # TODO:トランザクション
-    item = Item.new(
-      name: item_params[:name],
-      description: item_params[:description],
-      image: item_params[:image],
-      price: item_params[:price],
-      category_id: item_params[:category_id],
-      shipping_charge: item_params[:shipping_charge],
-      delivery_region: item_params[:delivery_region],
-      delivery_days: item_params[:delivery_days],
-      delivery_type: item_params[:delivery_type],
-      item_status: item_params[:item_status],
-      user_id: current_user.id,
-      trade_status: Item.trade_statuses[:showing])
+    Item.transaction do
+      item = Item.new(
+        name: item_params[:name],
+        description: item_params[:description],
+        image: item_params[:image],
+        price: item_params[:price],
+        category_id: item_params[:category_id],
+        shipping_charge: item_params[:shipping_charge],
+        delivery_region: item_params[:delivery_region],
+        delivery_days: item_params[:delivery_days],
+        delivery_type: item_params[:delivery_type],
+        item_status: item_params[:item_status],
+        user_id: current_user.id,
+        trade_status: Item.trade_statuses[:showing])
 
-    brand_name = item_params[:brand_id][:name]
-    if brand_name.present?
-      brand = Brand.find_or_initialize_by(name: brand_name)
-      brand.save unless brand.id?
-      item.brand_id = brand.id
+      brand_name = item_params[:brand_id][:name]
+      if brand_name.present?
+        brand = Brand.find_or_initialize_by(name: brand_name)
+        brand.save unless brand.id?
+        item.brand_id = brand.id
+      end
+
+      item.save!
     end
-
-    item.save
 
     # TODO:出品完了ページへ遷移
     redirect_to :root, notice: 'Item was successfully created.'
+
+    rescue => e
+      redirect_to :root, notice: 'Item was successfully created.'
   end
 
   def get_category_children
