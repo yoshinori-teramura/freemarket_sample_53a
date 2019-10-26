ff782c7327e7c917cd5f65bb774f2048ca330316
Shinichiro Miyazono <shinichiro.miyazono@gmail.com>
2019-10-26 19:27:07 +0900

    fix address tabel
    アドレスのスペルが一部間違っていたので修正しました

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -2,6 +2,9 @@ class BuyController < ApplicationController
 
   def show
     @items = Item.find(params[:id])
+
+    @address = Address.find(current_user.id)
+    
   end
 
 end
--- db/migrate/20190930113919_add_delivery_tel_to_address.rb
+++ db/migrate/20190930113919_add_delivery_tel_to_address.rb
@@ -1,4 +1,4 @@
-class AddDeliveryTelToAdress < ActiveRecord::Migration[5.2]
+class AddDeliveryTelToAddress < ActiveRecord::Migration[5.2]
   def change
     add_column :addresses, :delivery_tel, :integer
   end
