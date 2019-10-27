1cc8f0f1097a202f15eb8467835ae2afb4db64bc
skysan <skysan.justacase@gmail.com>
2019-10-28 01:03:05 +0900

    商品編集時のバリデーションを修正

--- app/assets/javascripts/sell.validate.js
+++ app/assets/javascripts/sell.validate.js
@@ -60,14 +60,84 @@ $(document).on('turbolinks:load', function () {
         rangelength: '1千万未満で金額設定してください'
       }
     },
-   
+
     errorPlacement: function(error, element){
       if (element.attr("name")=="item[image]"){
         error.appendTo($('#validation_image'));
       }else {
       error.insertAfter(element);
-      } 
-    }  
+      }
+    }
   });
 });
 
+
+$(document).on('turbolinks:load', function () {
+  $.validator.addMethod("boolCheck", function(value, element,origin){
+    return origin != value;
+  }, '商品画像は必須項目です。必ず一枚以上貼付してください。');
+
+  $('#selledititem').validate({
+    ignore: [],
+    errorClass: 'valid-err',
+    rules: {
+      "item[image_exists]": {
+        boolCheck: "false",
+      },
+      "item[name]": {
+        required: true
+      },
+      "item[description]": {
+        required: true
+      },
+      "item[category_id]": {
+        selectCheck: 0,
+        required: true
+      },
+      "category_root_id": {
+        selectCheck: 0
+      },
+      "category_child_id": {
+        selectCheck: 0
+      },
+      "category_grandchild_id": {
+        selectCheck: 0
+      },
+      "item[price]": {
+        required: true,
+        number: true,
+        rangelength: [1, 7]
+      },
+      "item[item_status]": {
+        selectCheck: 0
+      },
+      "item[shipping_charge]": {
+        selectCheck: 0
+      },
+      "item[delivery_type]": {
+        selectCheck: 0
+      },
+      "item[delivery_region]": {
+        selectCheck: 0
+      },
+      "item[delivery_days]": {
+        selectCheck: 0
+      }
+    },
+
+    messages: {
+      "item[price]": {
+        rangelength: '1千万未満で金額設定してください'
+      }
+    },
+
+    errorPlacement: function (error, element) {
+      if (element.attr("name") == "item[image_exists]") {
+        error.appendTo($('#validation_image'));
+      } else {
+        error.insertAfter(element);
+      }
+    }
+  });
+
+});
