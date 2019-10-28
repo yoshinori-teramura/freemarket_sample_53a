7fa205a8980177354f110cf0f8093fa54fe7653a
skysan <skysan.justacase@gmail.com>
2019-10-29 00:03:44 +0900

    出品中の商品のページ作成

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,21 @@
 class ListingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def listing
+    @items = current_user.items
+      .where(trade_status: :showing)
+      .limit(5)
+  end
+
+  def in_progress
+    @items = current_user.items
+      .where(trade_status: :trading)
+      .limit(5)
+  end
+
+  def completed
+    @items = current_user.items
+      .where(trade_status: :sold)
+      .limit(5)
+  end
 end
