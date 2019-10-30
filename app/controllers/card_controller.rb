cf87c2f558350495c47470ad72d91a75515b2de5
Shinichiro Miyazono <shinichiro.miyazono@gmail.com>
2019-10-30 20:20:30 +0900

    Revert "create card controller and views"
    This reverts commit a556349e1831ac31bbd3182048da56cba8306ddd.

--- app/controllers/card_controller.rb
+++
@@ -1,7 +0,0 @@
-class CardController < ApplicationController
-  def new
-  end
-
-  def show
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'card/new'
-  get 'card/show'
   devise_for :users,
   controllers: {
     sessions: 'users/sessions',
