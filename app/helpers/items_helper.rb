65ca0978bc0801a35e2ee4afa0bc3bd26a280b6b
skysan <skysan.justacase@gmail.com>
2019-11-08 01:40:47 +0900

    購入画面に進むボタンの判定を修正

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+
+  def can_buy_item(item)
+    return false if user_signed_in? == false
+    return false if item.user.id == current_user.id
+    return false if item.trade_status_showing? == false
+    return true
+  end
 end
