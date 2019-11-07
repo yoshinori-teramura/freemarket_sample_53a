ba1c91f036e5f90c1b1b11c2b7a86ab916f0cc7c
skysan <skysan.justacase@gmail.com>
2019-11-08 03:47:06 +0900

    buyerの単体テスト追加

--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Buyer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    FactoryBot.create(:item)
+  end
+
+  describe '#create' do
+    it "正常系" do
+      buyer = FactoryBot.build(:buyer, user_id:"1", item_id:"1")
+      expect(buyer).to be_valid
+    end
+
+    it "存在しないユーザ" do
+      buyer = FactoryBot.build(:buyer, user_id:"2", item_id:"1")
+      buyer.valid?
+      expect(buyer.errors[:user]).to include("を入力してください")
+    end
+
+    it "存在しない商品" do
+      buyer = FactoryBot.build(:buyer, user_id:"1", item_id:"3")
+      buyer.valid?
+      expect(buyer.errors[:item]).to include("を入力してください")
+    end
+
+  end
+
 end
