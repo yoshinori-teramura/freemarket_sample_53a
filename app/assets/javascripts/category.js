7b713fc1a2f00a2322382deeb00c5cce632ad074
yoshinori-teramura <51747559+yoshinori-teramura@users.noreply.github.com>
2019-11-08 23:33:21 +0900

    Update category.js

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,17 +1,25 @@
-$(document).on('turbolinks:load', function () {
-  $(function() {
-    // mouseoverを使用
-    $(".category_choice__box").hover(function() {
-      $(this).children('.category_choice-sub:not(:animated)').slideToggle();    
-    });
+
+$(document).on('turbolinks:load', function(){
+  $(".category_choice-menu").hide();
+  $(".header__bottom__left__list__link").hover(function() {
+    $('.category_choice-menu:not(:animated)').slideDown("fast")
+    },
+    function(){
+      $('.category_choice-menu').hover(function(){
+      },
+      function(){
+      $(".category_choice-menu").hide();
+      });
   });
 });
 
-$(document).on('turbolinks:load', function(){
-  $(function() {
-    // mouseoverを使用
-    $(".header__bottom__left__list__link").hover(function() {
-      $('.category_choice-menu:not(:animated)').slideToggle();    
-    });
+
+$(document).on('turbolinks:load', function () {
+  $(".category_choice-sub").hide();
+  $(".category_choice__box").hover(function() {
+    $(this).children('.category_choice-sub:not(:animated)').slideDown('fast')
+    },
+    function(){
+      $('.category_choice-sub',this).slideUp("fast");    
   });
 });
