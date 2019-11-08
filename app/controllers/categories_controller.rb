0b832ae240a6b75ee3bc14cfc3343e31d309abe6
Shinichiro Miyazono <shinichiro.miyazono@gmail.com>
2019-11-08 20:39:40 +0900

    update show.html.haml
    showページのビューとコントローラーをアップデートしました

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @items = Item.where(category_id: @category.leaves).page(params[:page]).per(8).order("created_at DESC")
+    @items = Item.where(category_id: @category.id).page(params[:page]).per(8).order("created_at DESC")
   end
 
 end
